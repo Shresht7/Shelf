939f4b6cfb90c0146996fc951745684ed2fdd525
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-06-24 12:20:05 +0530

    Create a super simple page to test functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    let items = $state<string[]>([]);
+    let url = $state("");
+
+    function saveUrl(e: SubmitEvent) {
+        e.preventDefault();
+        items.push(url);
+    }
+</script>
+
+<form onsubmit={saveUrl}>
+    <input
+        id="url"
+        type="text"
+        placeholder="Save a URL. https://..."
+        bind:value={url}
+    />
+    <button type="submit">Save</button>
+</form>
+
+<main>
+    {#each items as item}
+        <p>{item}</p>
+    {/each}
+</main>
