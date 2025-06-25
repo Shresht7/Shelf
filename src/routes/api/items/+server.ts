b0b9df171f041e373ef72a3f24f09b7ea10b4b68
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-06-25 20:11:44 +0530

    Implement GET handler to retrieve all items from the database

---
+++ src/routes/api/items/+server.ts
@@ -0,0 +1,12 @@
+// Library
+import { getAllItems } from '$lib/server/database/item'
+import { json, type RequestHandler } from '@sveltejs/kit'
+
+export const GET: RequestHandler = async ({ request }) => {
+    try {
+        const items = await getAllItems()
+        return json(items)
+    } catch (e) {
+        return new Response('Failed to get all items', { status: 500 })
+    }
+}
